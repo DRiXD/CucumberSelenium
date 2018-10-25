805c22a755f09b12ed2bfb86fb819b2c3498b3f3
DRiXD <feuriegel1@web.de>
2018-10-25 20:05:09 +0200

    implemented dhbwNavigationMenu testcase

---
+++ hellocucumber/src/test/java/hellocucumber/StepDefinitions/dhbwNavigationStepDefinitions.java
@@ -0,0 +1,34 @@
+package hellocucumber.StepDefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class dhbwNavigationStepDefinitions {
+
+    private String pathToChromeDriver= "C:\\Users\\Denis\\Documents\\DHBW\\3. Semester\\Software Engineering\\dev\\chromedriver_win321\\chromedriver.exe";
+    private WebDriver driver;
+
+    @Given("^I open the Chrome Browser and go to the Page \"([^\"]*)\"$")
+    public void iOpenTheChromeBrowserAndGoToThePage(String url) throws Throwable {
+        System.setProperty("webdriver.chrome.driver",pathToChromeDriver);
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(url);
+    }
+
+    @When("^I click on the navigation menu button$")
+    public void iClickOnTheNavigationMenuButton() throws Throwable {
+
+        driver.findElement(By.id("show-menu")).click();
+        Thread.sleep(1000);
+    }
+
+    @Then("^The navigation menu should scroll down$")
+    public void theNavigationMenuShouldScrollDown() throws Throwable {
+        driver.findElement(By.className("fixed"));
+    }
+}
---
+++ hellocucumber/src/test/resources/hellocucumber/dhbwNavigation.feature
@@ -0,0 +1,6 @@
+Feature: The Navigation Bar is there and can be openende
+  
+  Scenario: Open the Navigation Bar on DHBW Homepage
+    Given I open the Chrome Browser and go to the Page "http://www.karlsruhe.dhbw.de/startseite"
+    When I click on the navigation menu button
+    Then The navigation menu should scroll down
