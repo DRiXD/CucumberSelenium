689f532b51e51a33dbe72ee3b22667a5e567e389
DRiXD <feuriegel1@web.de>
2018-10-29 22:06:01 +0100

    cleaned up Project

---
+++ hellocucumber/src/test/java/hellocucumber/Calculator.java
@@ -0,0 +1,12 @@
+package hellocucumber;
+
+public class Calculator {
+
+    public int add(int firstNumber, int secondNumber){
+        return firstNumber+secondNumber;
+    }
+
+    public int multiplicate(int firstNumber, int secondNumber){
+        return firstNumber*secondNumber;
+    }
+}
---
+++ hellocucumber/src/test/java/hellocucumber/StepDefinitions/calculatorStepDefinitions.java
@@ -0,0 +1,25 @@
+package hellocucumber.StepDefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class calculatorStepDefinitions {
+
+    @Given("^I type the \"([^\"]*)\" into my calculator$")
+    public void iTypeTheIntoMyCalculator(String arg0) {
+
+    }
+
+    @When("^I multiplicate it with \"([^\"]*)\"$")
+    public void iMultiplicateItWith(String arg0) {
+
+    }
+
+    @Then("^My Calculator should return \"([^\"]*)\"$")
+    public void myCalculatorShouldReturn(String arg0) {
+
+    }
+
+}
